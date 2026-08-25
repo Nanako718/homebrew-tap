1c4558d2809422cb6ad21973ef0e1ba762013935
DTZSGHNR <2753086015@qq.com>
2026-08-25 22:56:46 +0800

    fix: 显式安装 Python 依赖 httpx, rich, plotext

--- Formula/speedtest-ustc.rb
+++ Formula/speedtest-ustc.rb
@@ -11,6 +11,9 @@ class SpeedtestUstc < Formula
 
   def install
     venv = virtualenv_create(libexec, "python3.12")
+    venv.pip_install "httpx"
+    venv.pip_install "rich"
+    venv.pip_install "plotext"
     venv.pip_install_and_link buildpath
   end
 
